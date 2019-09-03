d06dca5d64d7f04cd0a98593b97e3fd2be968e94
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-09-03 18:08:56 +0200

    Cleaner return with parentheses

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -10,20 +10,23 @@ function formatDate(date) {
 
 class UserInfo extends React.Component {
   render() {
-    return  <div className="UserInfo">
-              <Avatar user={this.props.user} />
-              <div className="UserInfo-name">
-                {this.props.user.name}
-              </div>
-            </div>
+    return (
+      <div className="UserInfo">
+        <Avatar user={this.props.user} />
+        <div className="UserInfo-name">
+          {this.props.user.name}
+        </div>
+      </div>
+    );
   }
 }
 class Avatar extends React.Component {
   render() {
-     return <img className="Avatar"
-          src={this.props.user.avatarUrl}
-          alt={this.props.user.name}
-        />
+     return (
+       <img className="Avatar"
+        src={this.props.user.avatarUrl}
+        alt={this.props.user.name} />
+     );
   }
 }
 
