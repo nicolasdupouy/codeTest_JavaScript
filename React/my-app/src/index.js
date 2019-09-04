8e1da43714e538adf90a1b7f49cf6a151239c5ad
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-09-04 18:08:23 +0200

    Manage clock ticking in Clock component state

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -3,21 +3,39 @@ import ReactDOM from 'react-dom';
 
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timedID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  tick() {
+    this.setState(
+      {date: new Date()}
+    );
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timedID);
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     )
   }
 }
 
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
