f07738e87234a2a80a94234d18f12bc8ed431d9f
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-09-04 11:36:16 +0200

    Make Clock component reusable (without state)

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function tick() {
-  const element = (
+
+function Clock(props) {
+  return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
-  );
+  )
+}
+
+function tick() {
   ReactDOM.render(
-    element,
+    <Clock date={new Date()} />,
     document.getElementById('root')
   );
 }
