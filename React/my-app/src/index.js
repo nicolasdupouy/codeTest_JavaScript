5bea1d510c8c4d26bc87222249767cb3ed36e01e
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-09-05 15:46:05 +0200

    Test Conditional Rendering (Element Variables)

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,33 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Toggle extends React.Component {
+const GREETING_LOGGED = 'Welcome back !';
+const GREETING_UNLOGGED = 'Please sign up.';
+
+class LoginControl extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {isToggleOn: true};
+        this.state = {isLoggedIn: false};
 
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true})
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false})
     }
 
     render() {
+        let button = this.state.isLoggedIn 
+            ? <LogoutButton clickCallback={this.handleLogoutClick} />
+            : <LoginButton clickCallback={this.handleLoginClick} />;
+
         return (
-            <button onClick={this.handleClick}>
-                {this.state.isToggleOn ? 'ON': 'OFF'}
+            <div>
+                <Greeting isLoggedIn={this.state.isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
+}
+
+class LoginButton extends React.Component {
+    render() {
+        return <LabelledButton label={'Login'} clickCallback={this.props.clickCallback} />;
+    }
+}
+
+class LogoutButton extends React.Component {
+    render() {
+        return <LabelledButton label={'Logout'} clickCallback={this.props.clickCallback} />;
+    }
+}
+
+class LabelledButton extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.clickCallback}>
+                {this.props.label}
             </button>
         );
     }
+}
 
-    handleClick() {
-        this.setState({
-            isToggleOn: !this.state.isToggleOn
-        });
-        // or
-        //this.setState(state => ({isToggleOn: !state.isToggleOn}));
+class Greeting extends React.Component {
+    render() {
+        const greetingMessage = this.props.isLoggedIn ? GREETING_LOGGED: GREETING_UNLOGGED;
+        return <h1>{greetingMessage}</h1>;
     }
 }
 
 ReactDOM.render(
-    <Toggle />, 
+    <LoginControl />, 
     document.getElementById('root')
 );
