74bca4ddc8c5c47dc4c6efadc9d615221f1eccbc
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-09-06 12:04:38 +0200

    Test Forms (Handling Multiple Inputs)

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -32,7 +32,64 @@ class NameForm extends React.Component {
     }
 }
 
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+        alert('isGoing: ' + this.state.isGoing + ' / numberOfGuests:' + this.state.numberOfGuests);
+        event.preventDefault();
+    }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
-    <NameForm />,
+    <div>
+        <NameForm />
+        <br /><hr />
+        <Reservation />
+    </div>,
     document.getElementById('root')
 );
